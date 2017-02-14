b8a4bb80492e8ddb0803e0065e75389eca61c5bc
Thomas Rodenhausen <thomas.rodenhausen@gmail.com>
2017-02-14 12:17:20 -0700

    initial

--- setupDB.sh
+++ setupDB.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 service mysql start
 mysqladmin -u root password my_secret_password
-mysql -uroot -pmy_secret_password -e "create database etcsite"
-mysql -uroot -pmy_secret_password -e "create database etcsite_alignment"
-mysql -uroot -pmy_secret_password -e "create database etcsite_charaparser"
-mysql -uroot -pmy_secret_password -e "create database etcsite_oto"
+mysql -uroot -pmy_secret_password -e "create database etcsite;"
+mysql -uroot -pmy_secret_password -e "create database etcsite_alignment;"
+mysql -uroot -pmy_secret_password -e "create database etcsite_charaparser;"
+mysql -uroot -pmy_secret_password -e "create database etcsite_oto;"
 mysql -uroot -pmy_secret_password etcsite < /root/git/etc-site/setupDB.sql
 mysql -uroot -pmy_secret_password etcsite_alignment < /root/git/euler/alignment/setupDB.sql
 mysql -uroot -pmy_secret_password etcsite_charaparser < /root/git/charaparser/setupDB.sql
