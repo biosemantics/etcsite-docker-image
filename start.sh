267b97ca42db801cf2fe117bd084090956e5c2bb
rodenhausen <thomas.rodenhausen@gmail.com>
2017-02-14 11:09:23 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,3 @@ service apache2 start
 
 mkdir /var/cache/jetty9/data
 cd /usr/share/jetty9; /usr/lib/jvm/java-8-openjdk-amd64/bin/java -Xmx256m -Djava.awt.headless=true 	-Djava.io.tmpdir=/var/cache/jetty9/data 	-Djava.library.path=/usr/lib 	-Djetty.home=/usr/share/jetty9 	-Djetty.logs=/var/log/jetty9 	-Djetty.state=/var/lib/jetty9/jetty.state -jar /usr/share/jetty9/start.jar jetty-started.xml    
-
-#service jetty9 start
-#while true; do sleep 10000; done
